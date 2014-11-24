00498af3de2a003da68c7e7983188ac15f1e2074
jlattimer <jason.lattimer@gmail.com>
2014-11-24 12:10:06 -0600

    https://github.com/jlattimer/CRMCreateAccountsWithNotesExample.git

---
+++ CRMCreateAccountsWithNotesExample/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xrm.Client;
+using Microsoft.Xrm.Client.Services;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Configuration;
+using System.Globalization;
+using System.Text;
+
+namespace CRMCreateAccountsWithNotesExample
+{
+    class Program
+    {
+        private static OrganizationService _orgService;
+
+        private const int NumberToCreate = 100;
+
+        static void Main()
+        {
+            CrmConnection connection = CrmConnection.Parse(
+                ConfigurationManager.ConnectionStrings["CRMOnlineO365"].ConnectionString);
+
+            using (_orgService = new OrganizationService(connection))
+            {
+                for (int i = 0; i < NumberToCreate; i++)
+                {
+                    Entity account = new Entity("account");
+                    account["name"] = i.ToString(CultureInfo.InvariantCulture);
+
+                    Guid id = _orgService.Create(account);
+
+                    Entity annotation = new Entity("annotation");
+                    annotation["objectid"] = new EntityReference("account", id);
+                    annotation["subject"] = "test";
+                    annotation["filename"] = "test.txt";
+                    annotation["documentbody"] = Convert.ToBase64String(
+                        new UnicodeEncoding().GetBytes("Sample Annotation Text"));
+                    annotation["mimetype"] = "text/plain";
+
+                    _orgService.Create(annotation);
+
+                    Console.WriteLine("Created: " + i);
+                }
+            }
+        }
+    }
+}
